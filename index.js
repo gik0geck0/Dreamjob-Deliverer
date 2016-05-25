447534a7b850afb99efde1fac98aef372ba79e4e
davisjk <davisjk972@gmail.com>
2016-05-25 15:50:38 -0600

    Created layout for user to view tests using bootstrap

--- index.js
+++ index.js
@@ -39,6 +39,10 @@ app.get('/db', function (request, response) {
 	});
 });
 
+app.get('/*', function (request, response) {
+	response.redirect('/');
+});
+
 app.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
 });
