ad992658be835f166c223a89d6b5c0991a6fc757
davisjk <davisjk972@gmail.com>
2016-05-25 18:14:22 -0600

    Time remaining functions

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -8,7 +8,39 @@ $(document).ready(function(){
 		$('#file_name').val(filename);
 	});
 	
-	$('#time_button').click(function(){
-		$('#time_remaining').html(new Date().getTime());
-	});
+	initializeClock('time_remaining', '5/26/2216 0:12:00 UTC');
 });
+
+function getTimeRemaining(endtime){
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor( (t/1000) % 60 );
+	var minutes = Math.floor( (t/1000/60) % 60 );
+	var hours = Math.floor( (t/(1000*60*60)) % 24 );
+	var days = Math.floor( t/(1000*60*60*24) );
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}
+
+function initializeClock(id, endtime){
+	var clock = document.getElementById(id);
+	var timeinterval = setInterval(function(){
+		var t = getTimeRemaining(endtime);
+		var remaining = "";
+		if (t.days > 0) remaining += t.days + 'd ';
+		if (t.hours > 0) remaining += t.hours + 'h ';
+		remaining += t.minutes + 'm ' + t.seconds + 's ';
+		if (t.total <= 0) {
+			remaining = "Ended";
+			$('#time_button').addClass('btn-danger').removeClass('btn-warning');
+		}
+		clock.innerHTML = remaining;
+		if(t.total<=0){
+			clearInterval(timeinterval);
+		}
+	},1000);
+}
