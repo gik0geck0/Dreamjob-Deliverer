5348e892684a6b7e3e53ee0eb61b07b61485c5c2
davisjk <davisjk972@gmail.com>
2016-06-09 17:06:11 -0600

    Admin page scheduled/in-prigress/finished tabs formatted and easy to add items.

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,23 +1,122 @@
-function loadList() {
-    $('#test_list').append("<a href='#' class='list-group-item'><span class='badge'>schedule</span> Item</a>");
+function loadList(testname) {
+    $('#test_list').append(
+		`<a href='/view?testname=${testname}' class='list-group-item'>
+			<span class='badge' href='/schedule?testname=${testname}'>schedule</span>
+			${testname}
+		</a>`
+	);
 }
 
-function addScheduled(candidate, title, start, end) {
-	$('#scheduled-tbody').append(`<tr><td>${candidate}</td><td>${title}</td><td>${end}</td></tr>`);
+function addScheduled(candidate, email, title, start, end) {
+	$('#scheduled-tbody').append(
+		`<tr>
+			<td>${candidate} (${email})</td>
+			<td>${title}</td>
+			<td>${start}</td>
+			<td>${end}</td>
+			<td class='glyph-td' title='Reschedule test times'>
+				<a href='#'>
+					<span class='glyph glyphicon glyphicon-calendar'></span>
+				</a>
+			</td>
+		</tr>`
+	);
 }
 
-function addInProgress(candidate, title, end) {
-	$('#scheduled-tbody').append(`<tr><td>${candidate}</td><td>${title}</td><td>${end}</td></tr>`);
+function addInProgress(candidate, email, title, start, end) {
+	$('#in-progress-tbody').append(
+		`<tr>
+			<td>${candidate} (${email})</td>
+			<td>${title}</td>
+			<td>${start}</td>
+			<td>${end}</td>
+			<td class='glyph-td' title='Reschedule test times'>
+				<a href='#'>
+					<span class='glyph glyphicon glyphicon-calendar'></span>
+				</a>
+			</td>
+		</tr>`
+	);
 }
 
-function addFinished(candidate, title, end) {
-	$('#scheduled-tbody').append(`<tr><td>${candidate}</td><td>${title}</td><td>${end}</td></tr>`);
+function addFinished(candidate, email, title, start, end) {
+	$('#finished-tbody').append(
+		`<tr>
+			<td>${candidate} (${email})</td>
+			<td>${title}</td>
+			<td>${start}</td>
+			<td>${end}</td>
+			<td class='glyph-td' title='Reschedule test times'>
+				<a href='#'>
+					<span class='glyph glyphicon glyphicon-calendar'></span>
+				</a>
+			</td>
+			<td class='glyph-td' title='Download test submission'>
+				<a href='#'>
+					<span class='glyph glyphicon glyphicon-save'></span>
+				</a>
+			</td>
+		</tr>
+	`);
 }
 
 $(document).ready(function(){
-	loadList();
-	loadList();
-	addInProgress('Tom', 'Test1', '07-05-2016');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	loadList('Test1');
+	loadList('Test2');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
+	addScheduled('Tom', 'tom@gmail.com', 'Test1', '07-05-2016 7:00 am', '07-15-2016 7:00 am');
+	addInProgress('Tom', 'tom@gmail.com', 'Test2', '06-05-2016 7:00 am', '07-05-2016 7:00 am');
+	addFinished('Tom', 'tom@gmail.com', 'Test1', '06-05-2016 7:00 am', '06-08-2016 9:00 am');
     
     $('#create-new-test').click(function(){
         var url = '/create';
@@ -31,6 +130,6 @@ $(document).ready(function(){
 
 $(window).on('resize', function() {
 	$('.scrolling-list').each(function(i, obj) {
-		$(obj).css('max-height', $(window).height()/3);
+		$(obj).css('max-height', ($(window).height()-$('#page-title').height()-$('#test-library-div').height()-150)/2);
 	});
 });
