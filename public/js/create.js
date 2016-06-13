ec5dad1a7c92859690ccb77bf203053a14216555
davisjk <davisjk972@gmail.com>
2016-06-13 13:48:15 -0600

    The PDF is not showing on the test page for some reason, I'm trying to fix it

--- public/js/create.js
+++ public/js/create.js
@@ -1,15 +1,14 @@
 var fileUploadText = 'Select an instructions file to upload';
 
 $(function() {
+	//File selection input
 	$('#file_name').click(function() {
 		$('#select_file').click();
 	});
-	
 	$('#select_file').change(function() {
 		var filename = $('#select_file').val().split('\\').pop().split('/').pop() || fileUploadText;
 		$('#file_name').val(filename);
 	});
-
 	$('#file_name').keydown(function(e){
 		if (e.which != 13 && e.which != 9)
 		e.preventDefault();
--- public/js/test.js
+++ public/js/test.js
@@ -104,13 +104,19 @@ function initializeClock(id, endtime){
 /* End time remaining stuff */
 
 $(function() {
+	var fileUploadText = 'Select an instructions file to upload';
+
 	//File selection input
 	$('#file_name').click(function() {
 		$('#select_file').click();
 	});
 	$('#select_file').change(function() {
-		var filename = $('#select_file').val().split('\\').pop().split('/').pop() || 'Select a file to upload';
+		var filename = $('#select_file').val().split('\\').pop().split('/').pop() || fileUploadText;
 		$('#file_name').val(filename);
+	});
+	$('#file_name').keydown(function(e){
+		if (e.which != 13 && e.which != 9)
+		e.preventDefault();
 	});
 	
 	//Start the timer until end time
